e0258c409aa353e43162f63cbf08668dbf884fd0
seireitou <ausis@mail.com>
2021-02-03 18:01:48 -0600

    defines ask_for_choice method in Cli class and request class method inside of Api class

--- lib/api.rb
+++ lib/api.rb
@@ -1,3 +1,8 @@
 class Api
-    
+    @@url = "https://ghibliapi.herokuapp.com/films"
+
+    def self.request
+        response = HTTParty.get(@@url)
+        binding.pry
+    end
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -2,5 +2,16 @@ class Cli
     def welcome
         puts ""
         puts "Welcome To The Ghibli Searcher!"
+        self.ask_for_choice
+    end
+
+    def ask_for_choice
+        puts "What would you like to search for?"
+        puts "1. Films"
+        puts "2. People"
+        puts "3. Vehicles"
+        # input = gets.strip
+        #  do something with choice
+        Api.request
     end
 end
